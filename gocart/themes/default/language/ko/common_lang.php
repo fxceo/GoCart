dfa7c492bd00a044c751b7b2a3bea09053fcf0a5
unknown <david.kim@funexlab.com>
2014-06-23 14:05:08 +0900

    Add Koean for default theme

---
+++ gocart/themes/default/language/ko/common_lang.php
@@ -0,0 +1,255 @@
+<?php
+
+/******************************************
+
+Korean
+Common Language
+
+******************************************/
+
+
+
+
+/*******************
+
+Site front end
+
+*******************/
+
+//common terms
+$lang['login']					= '로그인';
+$lang['logout']					= '로그아웃';
+$lang['my_account']				= '내 계정';
+$lang['my_downloads']			= '내 다운로드';
+$lang['enabled']				= 'Enabled';
+$lang['disabled']				= 'Disabled';
+$lang['mode']					= '모드';
+$lang['currency']				= '통화';
+$lang['active']					= 'Active';
+$lang['yes']					= '예';
+$lang['no']						= '아니오';
+$lang['download_link']			= 'Click here to access your downloads!';
+$lang['catalog']				= '카탈로그';
+$lang['account']				= '계정';
+$lang['close']					= '닫기';
+
+$lang['empty_cart']				= '장바구니가 비어있습니다';
+$lang['single_item']			= '장바구니에 %d개의 상품이 있습니다';
+$lang['multiple_items']			= '장바구니에 %d개의 상품이 있습니다';
+$lang['empty_view_cart']		= '장바구니에 상품이 없습니다!';
+$lang['your_cart']				= '장바구니';
+
+$lang['on_sale']				= '세일중!';
+$lang['related_products_title']	= '이런 상품은 어떤가요?'; // You might also like...
+$lang['form_submit']			= '확인';
+$lang['form_register']			= '회원가입';
+$lang['form_login']				= '로그인';
+$lang['reset_password']			= '비밀번호 재설정';
+$lang['form_search']			= '검색';
+$lang['form_cancel']			= '취소';
+$lang['form_add_to_cart']		= '장바구니에 담기';
+$lang['form_update_cart']		= '다시 계산';
+$lang['form_checkout']			= '주문하기';
+$lang['form_continue']			= '계속';
+$lang['form_previous']			= 'Edit Billing';
+$lang['form_choose']			= '선택';
+$lang['form_edit']				= '수정';
+$lang['form_delete']			= '삭제';
+$lang['form_search']			= '검색';
+$lang['form_view']				= 'View';
+$lang['form_save']				= '저장';
+$lang['form_from']				= 'From';
+$lang['form_to']				= 'to';
+
+$lang['step_1']					= 'Step 1';
+$lang['step_2']					= 'Step 2';
+$lang['free_shipping_basic']	= '무료배송 (basic)';
+
+$lang['search']					= '검색';
+$lang['search_error']			= 'You did not supply a search term';
+$lang['amount']					= 'Amount';
+$lang['custom_amount']			= 'Custom Amount';
+$lang['preset_amount']			= 'Preset Amount';
+$lang['recipient_name']			= 'Recipient Name';
+$lang['recipient_email']		= 'Recipient Email';
+$lang['sender_email']			= 'Sender Email';
+$lang['custom_greeting']		= 'Custom Greeting Message';
+$lang['check_out']				= 'Check Out';
+
+$lang['sort_by_name_asc']		= '상품명 오름차순';
+$lang['sort_by_name_desc']		= '상품명 내림차순';
+$lang['sort_by_price_asc']		= '가격 낮은순';
+$lang['sort_by_price_desc']		= '가격 높은순';
+
+$lang['products']				= '상품';
+$lang['out_of_stock']			= '품절';
+$lang['no_products']			= '현재 이 카테고리에 등록된 상품이 없습니다.';
+$lang['no_image_available']		= '이미지가 없습니다.';
+$lang['product_reg']			= '정상가:';
+$lang['product_price']			= '정상가:';
+$lang['product_sale']			= '할인가:';
+$lang['available_options']		= '가능한 옵션';
+$lang['choose_option']			= '옵션을 선택하세요';
+$lang['tab_description']		= 'Description';
+$lang['tab_related_products']	= 'Related Products';
+$lang['subcategories']			= 'Subcategories';
+$lang['edit_category']			= 'Edit Category';
+$lang['edit_product']			= 'Edit Product';
+
+$lang['loading']					= 'Loading&hellip;';
+$lang['coupon_label']				= '쿠폰이 있으시면 여기에 코드를 입력하세요:';
+$lang['gift_card_label']			= '상품권이 있으시면 여기에 코드를 입력하세요:';
+$lang['apply_gift_card']			= '상품권 적용';
+$lang['apply_coupon']				= '쿠폰 적용';
+$lang['customer_information']		= 'Customer Information';
+$lang['error_save_payment']			= '배송 방법을 저장하는 중에 오류가 발생하였습니다';
+$lang['error_choose_payment']		= '결제 방법을 선택해 주세요';
+$lang['error_choose_shipping']		= '배송 방법을 선택해 주세요';
+$lang['continue_shopping']			= '쇼핑 계속하기';
+$lang['register_now']				= '가입하기';
+$lang['submit_order']				= '주문하기';
+$lang['edit_customer_information']	= 'Edit Customer Information';
+$lang['communication_error']		= '서버와 통신하는 중 예상치 못한 문제가 발생했습니다.';
+$lang['no_payment_needed']			= '결제가 필요하지 않습니다.';
+$lang['choose_payment_method']		= '결제 방법을 선택하세요.';
+$lang['shipping_method']			= '배송 방법';
+$lang['no_shipping_needed']			= 'Your order does not include any items that require shipping.';
+$lang['payment_information']		= '결제 정보';
+$lang['payment_method']				= '결제 방법';
+$lang['submit_payment_method']		= 'Submit Payment Method';
+$lang['product_information']		= '상품정보';
+$lang['price_and_quantity']			= '가격 &amp; 수량';
+$lang['sku']						= 'SKU';
+$lang['name']						= '상품명';
+$lang['price']						= '가격';
+$lang['description']				= '설명';
+$lang['quantity']					= '수량';
+$lang['totals']						= '합계';
+$lang['group_discount']				= '그룹 할인';
+$lang['subtotal']					= '소계';
+$lang['cart_total']					= '합계';
+$lang['coupon_discount']			= '쿠폰 할인';
+$lang['gift_card_discount']			= '상품권 할인';
+$lang['discounted_subtotal']		= '할인 소계';
+$lang['shipping']					= '배송';
+$lang['tax']						= '세금';
+$lang['taxes']						= '세금';
+$lang['grand_total']				= '합계';
+$lang['address_manager']			= '주소 관리자';
+$lang['address_form']				= '주소 정보';
+$lang['your_addresses']				= '주소록';
+$lang['address_button']				= '주소 변경';
+$lang['shipping_address_button']	= '배송지 주소 변경';
+$lang['billing_address_button']		= '결제 주소 변경';
+$lang['shipping_method_button']		= '배송 방법 변경';
+$lang['billing_method_button']		= '결제 방법 변경';
+$lang['shipping_and_billing']		= '배송 &amp; 결제 정보';
+$lang['billing_address']			= '결제 주소';
+$lang['choose_billing_address']		= '결제 주소를 선택해주세요';
+$lang['use_address_for_shipping']	= '이 주소로 배송하기';
+$lang['shipping_address']			= '배송 주소';
+$lang['shipping_information']		= '배송 정보';
+$lang['billing_information']		= '결제 정보';
+$lang['choose_shipping_address']	= '배송지 주소를 선택하세요';
+$lang['choose_address']				= '주소 선택';
+$lang['ship_to_address']			= '이 주소로 배송합니다';
+$lang['contact_info']				= 'Contact Info';
+$lang['address_company']			= '회사';
+$lang['address']					= '주소';
+$lang['address1']					= '주소 1';
+$lang['address2']					= '주소 2';
+$lang['address_firstname']			= '이름';
+$lang['address_lastname']			= '성';
+$lang['address_email']				= '이메일';
+$lang['address_phone']				= '전화번호';
+$lang['address_city']				= '도시';
+$lang['address_zip']				= '우편번호';
+$lang['address_country']			= '국가';
+$lang['address_state']				= 'State';
+$lang['message_address_saved']		= 'Your address has been saved!';
+
+$lang['default']					= '기본';
+$lang['additional_order_details']	= 'Additional Order Details';
+$lang['additional_details']			= 'Additional Details';
+$lang['heard_about']				= 'How did you hear about us?';
+$lang['shipping_instructions']		= 'Shipping Notes';
+
+//checkout confirmation
+$lang['thank_you']					= 'Thanks for shopping with '; // terminates with company/site name, defined in gocart config as 'company_name'
+
+//authorize.net library messages
+$lang['please_fix_errors']				= 'Please fix the following errors:';
+$lang['enter_card_name']				= 'Please enter your first and last name as it appears on the card';
+$lang['invalid_card_exp']				= 'The expiration date does not appear to be valid';
+$lang['invalid_card_num']				= 'The card number you entered is not a valid credit card number';
+$lang['enter_card_code']				= 'Please enter the three digit security code on the reverse side of the card';
+$lang['transaction_declined']			= 'Transaction Declined. Please check your card information and try again.';
+$lang['enter_test_mode_credentials']	= 'You must enter login values for TEST mode';
+$lang['enter_live_mode_credentials']	= 'You must enter login values for LIVE mode';
+$lang['all_required_fields']			= 'Please complete all required fields.';
+$lang['card_number']					= 'Card Number';
+$lang['expires_on']						= 'Expires On';
+$lang['cvv_code']						= 'CVV Code';
+
+//giftcards
+$lang['giftcard']					= 'Gift Card';
+$lang['giftcard_choose_amount']		= 'Choose Gift Card Amount';
+$lang['giftcard_custom_amount']		= 'Custom Amount';
+$lang['giftcard_to']				= 'To (Name)';
+$lang['giftcard_email']				= 'To (Email)';
+$lang['giftcard_from']				= 'From';
+$lang['giftcard_message']			= 'Personal Message';
+$lang['giftcard_excerpt']			= 'Gift Card for a friend<br/>To: %s';
+
+$lang['email']						= '이메일';
+$lang['password']					= '비밀번호';
+$lang['confirm_password']			= 'Confirm Password';
+$lang['return_to_login']			= 'Return to Login';
+$lang['forgot_password']			= '비밀번호 찾기';
+$lang['keep_me_logged_in']			= '로그인 상태 유지하기';
+$lang['register']					= '회원가입';
+$lang['login_failed']				= 'Authentication Failed!';
+$lang['go_to_login']				= '로그인';
+
+$lang['account_registration']			= 'Account Registration';
+$lang['registration_thanks']			= 'Thanks for registering %s!';
+$lang['error_email']					= 'The requested email is already in use.';
+$lang['error_no_account_record']		= 'There is no record of your account.';
+$lang['message_new_password']			= 'A new password has been generated and sent to your email.';
+$lang['message_account_updated']		= '계정 정보가 변경되었습니다';
+
+$lang['error_invalid_shipping_method']		= '올바른 배송 방법을 선택해주세요.';
+$lang['delete_address_confirmation']		= '정말 이 주소를 삭제하시겠습니까?';
+$lang['error_must_have_address']			= '주소 관리자에 최소한 한 개의 주소를 남겨두어야 합니다.';
+$lang['account_information']				= '계정 정보';
+$lang['account_company']					= '회사명';
+$lang['account_firstname']					= '이름';
+$lang['account_lastname']					= '성';
+$lang['account_email']						= '이메일';
+$lang['account_phone']						= '전화번호';
+$lang['account_password']					= '비밀번호';
+$lang['account_confirm']					= '비밀번호 확인';
+$lang['account_newsletter_subscribe']		= '뉴스레터를 수신합니다'; // Subscribe to our email list
+$lang['account_password_instructions']		= '비밀번호 변경을 원치 않으시면 빈칸으로 남겨두세요.'; //If you do not wish to change your<br/>password, leave both fields blank.'
+$lang['default_billing']					= '기본 결제';
+$lang['default_shipping']					= '기본 배송';
+$lang['order_history']						= '주문 내역';
+$lang['order_date']							= '주문일';
+$lang['order_number']						= '주문번호';
+$lang['order_status']						= '상태';
+$lang['add_address']						= '주소추가';
+$lang['no_order_history']					= 'You have no orders in your account history.';
+
+//cart library messages
+$lang['coupon_not_apply']					= 'The coupon code you entered does not apply to any products in your cart.';
+$lang['coupon_applied']						= 'The coupon has been applied to your products.';
+$lang['coupon_invalid']						= 'Sorry, but the coupon you entered is not valid at this time.';
+$lang['coupon_already_applied']				= 'That coupon code has already been applied to your items.';
+$lang['invalid_coupon_code']				= 'There was no coupon with that code. Check to make sure you entered it correctly.';
+$lang['giftcard_balance_applied']			= 'You should see your card balance displayed.';
+$lang['giftcard_zero_balance']				= 'Sorry, but the gift card you entered is expired or has a zero balance.';
+$lang['giftcard_not_exist']					= 'The gift card code you entered does not exist.';
+$lang['giftcard_already_applied']			= 'Your gift card is already applied';
+$lang['error_updating_cart']				= 'There was an error updating the cart!';
+$lang['not_enough_stock']					= 'The quantity requested for the "%s" product could not be added. We only have %d item(s) in stock.';
+$lang['remove_item']						= '정말 장바구니에서 이 상품을 제거하시겠습니까?';
